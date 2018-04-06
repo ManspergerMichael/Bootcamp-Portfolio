5bdf8d0ae5f1f2b620771296f1c7017be17f607d
ManspergerMichael <ManspergerMichael@gmail.com>
2018-04-05 18:25:11 -0700

    Whats my name?

--- Python/python_stack/flask_fundamentals/WhatsMyName/server.py
+++ Python/python_stack/flask_fundamentals/WhatsMyName/server.py
@@ -1,14 +1,16 @@
 from flask import Flask, render_template, request, redirect
 app = Flask(__name__) 
-
+#directs to page with form
 @app.route('/')
 def landing():
     return render_template('index.html')
-
+#takes in data from form in index.html prints form data to terminal
 @app.route('/process', methods=['POST'])
 def submit():
-    for x in request.form:
-        print request.form
+    #data sent from index.html is in the request object.form dictionary
+    #dictionary keys will have the same name as the input field name properties
+    print request.form['fName']
+    print request.form['lName']
     return redirect('/')
 
 
