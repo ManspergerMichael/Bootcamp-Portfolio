3d2fca859e72e03841b00a4c84f3a3d8e571813a
ManspergerMichael <ManspergerMichael@gmail.com>
2018-04-07 17:33:34 -0700

    sill in error

--- Python/python_stack/flask_fundamentals/DojoServey Validation/server.py
+++ Python/python_stack/flask_fundamentals/DojoServey Validation/server.py
@@ -9,17 +9,18 @@ def landing():
 #takes in data from form in index.html prints form data to terminal
 @app.route('/result', methods=['POST'])
 def submit():
-    if len(request.form['name']) < 1:
+    if len(request.form['Name']) <= 0:
         flash("Name cannot be empty!")
         print "first statement run"
-        return redirect('/')
-    if len(request.form('comment')) > 1:
+        return render_template('index.html')
+    if len(request.form['comment']) > 1:
         flash("Comment must be under 120 characters!")
         print "second statement"
-        return redirect('/')
+        return render_template('index.html')
     return redirect('result.html',name = request.form['Name'],
-    location = request.form['Location'],langwage = request.form['Langwage'],
-    comment = request.form['comment'])
+                                location = request.form['Location'],
+                                langwage = request.form['Langwage'],
+                                comment = request.form['comment'])
 
 
 app.run(debug=True) 
