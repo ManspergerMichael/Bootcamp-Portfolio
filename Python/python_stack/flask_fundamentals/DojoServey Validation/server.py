c0ab58636faa6c3bdaaffee3c2ac6281258f376c
ManspergerMichael <ManspergerMichael@gmail.com>
2018-04-07 17:08:14 -0700

    Dojo Serv validation ERROR

---
+++ Python/python_stack/flask_fundamentals/DojoServey Validation/server.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, redirect, flash
+app = Flask(__name__) 
+#directs to page with form
+@app.route('/')
+def landing():
+    return render_template('index.html')
+#takes in data from form in index.html prints form data to terminal
+@app.route('/result', methods=['POST'])
+def submit():
+    if len(request.form['name']) < 1:
+        flash("Name cannot be empty!")
+        print "first statement run"
+        return redirect('/')
+    if len(request.form('comment')) > 1:
+        flash("Comment must be under 120 characters!")
+        print "second statement"
+        return redirect('/')
+    return redirect('result.html',name = request.form['Name'],
+    location = request.form['Location'],langwage = request.form['Langwage'],
+    comment = request.form['comment'])
+
+
+app.run(debug=True) 
