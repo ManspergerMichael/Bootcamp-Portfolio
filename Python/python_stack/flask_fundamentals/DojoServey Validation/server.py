5a17d8b6cf3eec37a4d412591f34cff90ee25ace
ManspergerMichael <ManspergerMichael@gmail.com>
2018-04-07 17:23:17 -0700

    sill in error

--- Python/python_stack/flask_fundamentals/DojoServey Validation/server.py
+++ Python/python_stack/flask_fundamentals/DojoServey Validation/server.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, redirect, flash
 app = Flask(__name__) 
+app.secret_key = "Bazinga!"
+
 #directs to page with form
 @app.route('/')
 def landing():
