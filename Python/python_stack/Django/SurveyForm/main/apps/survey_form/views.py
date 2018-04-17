92009fae5551e382467b7756e77a082308405610
ManspergerMichael <ManspergerMichael@gmail.com>
2018-04-17 11:01:45 -0700

    Amadon complete

--- Python/python_stack/Django/SurveyForm/main/apps/survey_form/views.py
+++ Python/python_stack/Django/SurveyForm/main/apps/survey_form/views.py
@@ -5,17 +5,18 @@ def survey(request):
     #print "Got here!"
     return render(request,"survey.html")
 def result(request, methods="POST"):
+    if request.method =='POST':
     #print "Result page"
     #print request.POST['name']
     #print request.POST['location']
     #print request.POST['language']
     #print request.POST['comment']
     
-    data = {
-        'name' : request.POST['name'],
-        'location' : request.POST['location'],
-        'language' : request.POST['language'],
-        'comment' : request.POST['comment']
-    } 
+        data = {
+            'name' : request.POST['name'],
+            'location' : request.POST['location'],
+            'language' : request.POST['language'],
+            'comment' : request.POST['comment']
+        } 
     
     return render(request, "result.html", data)
