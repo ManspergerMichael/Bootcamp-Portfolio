711096192532e973a7434083c32a9545e3d1bb35
ManspergerMichael <ManspergerMichael@gmail.com>
2018-04-20 17:04:56 -0700

    bunch of things done

--- Python/python_stack/Django/semi-restful-users/main/apps/semiRestfulUsers/views.py
+++ Python/python_stack/Django/semi-restful-users/main/apps/semiRestfulUsers/views.py
@@ -5,23 +5,37 @@ from django.core.urlresolvers import reverse
 # Create your views here.
 
 def index(request):
-    context = {
-        "user":users.objects.all()
-    }
-    
+    context = {"user":users.objects.all()}
     return render(request, 'index.html', context)
-def show(request, methods="POST"):
-    pass
-def new(request, methods="POST"):
-    responce = "placeholder for new user"
-    return HttpResponse(responce)
 
-def edit(request, methods="POST"):
-    user = users.objects.get(id = request.POST['id'])
-    user.first_name = request.POST['first_name']
-    user.last_name = request.POST['last_name']
-    user.email = request.POST['email']
-    user.save()
-    return redirect('target/semiRestfulUsers/index')
-def delete(request, methods="POST"):
-    pass
+def show(request, id, methods="POST"):
+    context = {
+        'user' : users.objects.get(id = id)
+    }
+    return render(request, 'show.html', context)
+
+def create(request, methods="POST"):
+    users.objects.create(first_name = request.POST['first_name'], last_name = request.POST['last_name'], email = request.POST['email'])
+    return redirect(reverse('index'))
+
+def new(request):
+    return render(request, 'new.html')
+
+def editPage(request, id, methods="POST"):
+    context = {
+        'user' : users.objects.get(id = id)
+    }
+    return render(request, 'edit.html', context)
+
+def editprocess(request, id, methods="POST"):
+    this_user = users.objects.get(id = id)
+    this_user.first_name = request.POST['first_name']
+    this_user.last_name = request.POST['last_name']
+    this_user.email = request.POST['email']
+    this_user.save()
+
+    return redirect(reverse('index'))
+def delete(request, id):
+    this_user = users.objects.get(id = id)
+    this_user.delete()
+    return redirect(reverse('index'))
