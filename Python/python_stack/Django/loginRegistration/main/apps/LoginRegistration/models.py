8a1d517e5f15f73308bf6fcbc15a99fc69f69d30
ManspergerMichael <ManspergerMichael@gmail.com>
2018-04-22 16:38:16 -0700

    loginregistration try catch

--- Python/python_stack/Django/loginRegistration/main/apps/LoginRegistration/models.py
+++ Python/python_stack/Django/loginRegistration/main/apps/LoginRegistration/models.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
-
+from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
-import re
+import re, bcrypt
 
 NAME_REGEX = re.compile(r'^[a-zA-Z]+$')
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
@@ -10,27 +10,44 @@ EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 class usersManager(models.Manager):
     def user_validator(self, postData):
         #errors dictionary for passing error messages to template
-        errors ={
-            
-        }
+        errors ={}
         #validation
-        if len(postData['first_name'])< 2:
-            errors['first_name'] = "First name should be more than 2 characters"
-            print "I found a validation error"
-        if len(postData['last_name'])< 2:
-            errors['Last Name'] = "Llast name should be more than 2 characters"
-            print "I found a validation error"
-        if not NAME_REGEX.match(postData['first_name']):
-            errors['First name Format'] = "First name can only have characters"
-            print "I found a validation error"
-        if not NAME_REGEX.match(postData['last_name']):
-            errors["Last Name Format"] = "Last name can only have characters"
-            print "I found a validation error"
-        if not EMAIL_REGEX.match(postData['email']):
-            errors['Email Format'] = "Email is not in proper format"
-            print "I found a validation error"
-        if len(postData['password']) < 8:
-            errors['Password Length'] = "Password should be longer than 8 characters"
+        if postData['type'] == 'register':
+            print "Register validation"
+            try:
+                test = users.objects.get(email = postData['email'])
+            except Exception:
+                errors['Email error'] = 'Email address is allready registered'
+            if len(postData['first_name'])< 2:
+                errors['first_name'] = "First name should be more than 2 characters"
+                print "I found a validation error"
+            if len(postData['last_name'])< 2:
+                errors['Last Name'] = "Llast name should be more than 2 characters"
+                print "I found a validation error"
+            if not NAME_REGEX.match(postData['first_name']):
+                errors['First name Format'] = "First name can only have characters"
+                print "I found a validation error"
+            if not NAME_REGEX.match(postData['last_name']):
+                errors["Last Name Format"] = "Last name can only have characters"
+                print "I found a validation error"
+            if not EMAIL_REGEX.match(postData['email']):
+                errors['Email Format'] = "Email is not in proper format"
+                print "I found a validation error"
+            if postData['password'] != postData['Confirm_pw']:
+                errors['Password'] = 'Passwords do not match'
+            if len(postData['password']) < 8:
+                errors['Password Length'] = "Password should be longer than 8 characters"
+        
+        elif postData['type'] == 'login':
+            print "login validation"
+            try:
+                test = users.objects.get(email = postData['email'])
+            except ObjectDoesNotExist:
+                errors['Email error'] = 'Email address is not registered'
+            test = users.objects.get(email = postData['email'])
+            if not bcrypt.checkpw(postData['password'].encode(), test.salt.encode()):
+                errors['Password Error'] ='Password is invalid'
+        
         
         return errors
 
@@ -41,7 +58,6 @@ class users(models.Model):
     email = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
     salt = models.CharField(max_length=255)
-    
     def __repr__():
         print self.first_name, self.last_name, self.email, self.password, self.salt
     #this includes the users manager object in the user object, extends the functionality of the object
--- Python/python_stack/Django/loginRegistration/main/apps/LoginRegistration/views.py
+++ Python/python_stack/Django/loginRegistration/main/apps/LoginRegistration/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, HttpResponse, redirect
 from .models import *
 from django.core.urlresolvers import reverse
 from django.contrib import messages
+from django.core.exceptions import ObjectDoesNotExist
 import bcrypt
 
 # Create your views here.
@@ -11,8 +12,6 @@ def landing(request):
 
 def register(request, methods="POST"):
     print "Im in register!"
-    if request.POST['password'] != request.POST['Confirm_pw']:
-        messages.error(request,'Passwords do not match',extra_tags ='Password')
     errors = users.objects.user_validator(request.POST)
     if len(errors):
         for tag, error in errors.iteritems():
@@ -22,25 +21,26 @@ def register(request, methods="POST"):
     else:
         #encript password
         hash1 = bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt())
-        print hash1
+        #print hash1
         users.objects.create(first_name = request.POST['first_name'], last_name = request.POST['last_name'], email = request.POST['email'], password = request.POST['password'], salt = hash1)
+        
         this_user = users.objects.get(email = request.POST['email'])
         id = this_user.id
     return redirect('sucsess', id =id)
 
 def login(request, methods="POST"):
-    user_email = str(request.POST['email'])
-    password = str(request.POST['password'])
-    try:
+    user_email = request.POST['email']
+    password = request.POST['password']
+    errors = users.objects.user_validator(request.POST)
+    if len(errors):
+        for tag, error in errors.iteritems():
+            print "I found an error message"
+            messages.error(request, error, extra_tags=tag)#this creates the key value pais for error messages
+        return redirect('/index')
+    else:
         this_user = users.objects.get(email = user_email)
-    except DoesNotExist:
-        messages.error(request,'Email address is not registered',extra_tags ='Login Error')
-        return redirect('/index')
-    if not bcrypt.checkpw(password.encode(), this_user.salt.encode()):
-        messages.error(request,'Password is invalid',extra_tags ='Password Error')
-        return redirect('/index')
-    id = this_user.id
-    return redirect('sucsess', id = id)
+        id = this_user.id
+        return redirect('sucsess', id = id)
 
 
 def success(request,id):
