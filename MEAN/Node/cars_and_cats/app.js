cdbec7d5621c2c361dd524ae11e21095ba37cad9
ManspergerMichael <ManspergerMichael@gmail.com>
2018-05-02 14:12:25 -0700

    cats and mechs

--- MEAN/Node/cars_and_cats/app.js
+++ MEAN/Node/cars_and_cats/app.js
@@ -10,8 +10,30 @@ var server = http.createServer(function (request, response){
         
         });
     }
-    /* to load a single image
-    else if(request.url === '/cars/ebj'){
+    else if(request.url === '/cats'){
+        fs.readFile('./views/cats.html','utf8', function(errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
+            response.write(contents);  //  send response body
+            response.end();
+        
+        });
+    }
+    else if(request.url === '/new'){
+        fs.readFile('./views/new.html','utf8', function(errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
+            response.write(contents);  //  send response body
+            response.end();
+        
+        });
+    }
+    else if(request.url === '/stylesheets/styles.css'){
+        fs.readFile('./stylesheets/styles.css', 'utf8', function(errors, contents){
+         response.writeHead(200, {'Content-type': 'text/css'});
+         response.write(""+contents);
+         response.end();
+        });
+      }
+    else if(request.url === '/images/EBJ.jpeg'){
         // notice we won't include the utf8 encoding
         fs.readFile('./images/EBJ.jpeg', function(errors, contents){
             response.writeHead(200, {'Content-type': 'image/jpg'});
@@ -19,7 +41,30 @@ var server = http.createServer(function (request, response){
             response.end();
         });
     }
-    */
+    else if(request.url === '/images/hunchback.jpeg'){
+        // notice we won't include the utf8 encoding
+        fs.readFile('./images/hunchback.jpeg', function(errors, contents){
+            response.writeHead(200, {'Content-type': 'image/jpg'});
+            response.write(contents);
+            response.end();
+        });
+    }
+    else if(request.url === '/images/BigLittleCats.jpeg'){
+        // notice we won't include the utf8 encoding
+        fs.readFile('./images/BigLittleCats.jpeg', function(errors, contents){
+            response.writeHead(200, {'Content-type': 'image/jpg'});
+            response.write(contents);
+            response.end();
+        });
+    }
+    else if(request.url === '/images/babyJag.jpeg'){
+        // notice we won't include the utf8 encoding
+        fs.readFile('./images/babyJag.jpeg', function(errors, contents){
+            response.writeHead(200, {'Content-type': 'image/jpg'});
+            response.write(contents);
+            response.end();
+        });
+    }
     else {
         response.writeHead(404);
         response.end('File not found!!!');
