449a9efc2f50305b8a4f24bb1b83ff9ab5dcf643
ManspergerMichael <ManspergerMichael@gmail.com>
2018-05-03 18:15:20 -0700

    survey form with errors

--- MEAN/Express/SurveyForm/server.js
+++ MEAN/Express/SurveyForm/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 let app = express();
 
 //render a page with a form
 app.set('views', path.join(__dirname + "/Views"))
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(request, response){
     console.log("root route");
@@ -13,8 +15,8 @@ app.get('/', function(request, response){
 
 app.post('/result', function(request, response){
     console.log("form posted");
-    console.log(request.body);
-    response.render('result',request.body)
+    console.log(request.body.name);
+    response.redirect('/', request)
 })
 app.listen(8000, function(errs){
     console.log("Server at 8000");
