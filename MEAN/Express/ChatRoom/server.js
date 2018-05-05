1366bf30ac7dde5c680b37f5b38c688b48b9a0b5
ManspergerMichael <ManspergerMichael@gmail.com>
2018-05-04 18:11:52 -0700

    Chat room almost complete

--- MEAN/Express/ChatRoom/server.js
+++ MEAN/Express/ChatRoom/server.js
@@ -3,12 +3,16 @@ const path = require('path');
 let app = express();
 const server = app.listen(8000);
 const io = require('socket.io')(server);
-app.set('views', path.join(__dirname + "/Views"))
+app.set('views', path.join(__dirname + "/views"))
+app.use(express.static(__dirname + "/static"));
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response){
     //console.log("root route");
     response.render('index');
+})
+app.get('/styles.css',function(request,response){
+    response.sendFile('styles.css',{root : __dirname + "./static/images"});
 })
 
 var users ={}
@@ -17,7 +21,7 @@ io.sockets.on('connection', function(socket){
     socket.on('registerUser', function(registeration){
         var username = registeration.name;
         users[socket.id] = username;
-        io.emit('msg', {msg:`${username} has joined the room`});
+        socket.broadcast.emit('msg', {msg:`${username} has joined the room`});
         //store new user in users
         //send new user to existing people in room
     });
