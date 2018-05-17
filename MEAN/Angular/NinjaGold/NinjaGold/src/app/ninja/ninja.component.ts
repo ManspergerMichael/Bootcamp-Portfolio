e9cb9056066b56b64fb652e9314da07e117f2cbc
ManspergerMichael <ManspergerMichael@gmail.com>
2018-05-17 14:07:15 -0700

    Shinto Coins

--- MEAN/Angular/NinjaGold/NinjaGold/src/app/ninja/ninja.component.ts
+++ MEAN/Angular/NinjaGold/NinjaGold/src/app/ninja/ninja.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NinjaService } from '../ninja.service';
-import { ActivatedRoute, Params} from '@angular/router'
+import { ActivatedRoute, Params} from '@angular/router';
 
 @Component({
   selector: 'app-ninja',
