1e69c7fa10c249db47afe71fcdfec1eefd69c741
ManspergerMichael <ManspergerMichael@gmail.com>
2018-06-08 16:14:40 -0700

    ModelView

---
+++ netCore/ViewModel/ViewModel/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ViewModel.Models;
+
+namespace ViewModel.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        [Route("")]
+        public IActionResult Message(){
+            Message mess = new Message();
+            mess.setMessage("hi");
+            return View("Message",mess);
+        }
+
+        
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ netCore/ViewModel/ViewModel/Models/Message.cs
@@ -0,0 +1,11 @@
+namespace ViewModel.Models{
+    public class Message{
+        private string message;
+        public string getMessage(){
+            return message;
+        }
+        public void setMessage(string mess){
+            this.message = mess;
+        }
+    }
+}
